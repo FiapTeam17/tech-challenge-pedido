cbb44c7a7ae7cde29beba6b39e0a7a41c4243781
Jefferson Nunes <jeffersonvnunes@gmail.com>
2024-01-20 16:30:16 -0300

    Melhorias na configuração do cucumber

--- cucumber.js
+++ cucumber.js
@@ -1,7 +1,12 @@
 module.exports = {
-  default: [
-    "test/features/*.feature",
-    "--require-module ts-node/register",
-    "--require test/features/**/*.ts",
-  ].join(" "),
-};
+  default: {
+    paths: ['test/features/**/*.feature'],
+    parallel: 2,
+    format: [
+      'progress-bar', 
+      ['html', 'cucumber-report.html']
+    ],
+    requireModule: ['ts-node/register'],
+    require: ['test/features/**/*.ts']
+  }
+}
